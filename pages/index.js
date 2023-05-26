3fa20fd56eec985ef0c4ee53655a64cc44855b29
jred9501 <jredondo9501@gmail.com>
2023-05-26 10:29:26 -0700

    grid layout

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,14 @@ import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div className="center">
-      <h2>Hello</h2>
-    </div>
+    <div className="container">
+    <div className="name-box">Navbar</div>
+    <div className="main">div</div>
+    <div id="sidebar">Sidebar</div>
+    {/* <div id="content1">Content1</div>
+    <div id="content2">Content2</div>
+    <div id="content3">Content3</div> */}
+    <footer>Footer</footer>
+  </div>
   );
 }
