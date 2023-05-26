cd866d93bf69b8484e060e7cbff80353b39cdc83
jred9501 <jredondo9501@gmail.com>
2023-05-26 09:20:59 -0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <div className="center">
-      <h2>Home Page</h2>
+      <h2>Hello</h2>
     </div>
   );
 }
